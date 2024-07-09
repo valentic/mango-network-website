1554a3de650e8540c024e59c8d7fb95633c94dd6
Todd Valentic <todd.valentic@gmail.com>
2024-07-09 18:16:32 +0000

    Move MDK to retired status

--- v1/devel/frontend/src/support/assets/retiredstations.js
+++ v1/devel/frontend/src/support/assets/retiredstations.js
@@ -1,6 +1,7 @@
 export const RetiredStations = [
     { name: 'brg', latitude: 45.29838, longitude: -108.92 },
     { name: 'frc', latitude: 33.29205, longitude: -89.386 },
-    { name: 'hco', latitude: 40.8,     longitude: -121.47 }
+    { name: 'hco', latitude: 40.8,     longitude: -121.47 },
+    { name: 'mdk', latitude: 38.11792, longitude: -96.09047 },
 ]
 
